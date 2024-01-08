84360b0fe5afaf81f4530f7fa8d235d54497fac5
naatbarcelos <naathalia.b@hotmail.com>
2024-01-08 20:59:34 -0300

    Creating Index HTML e JS

---
+++ index.js
@@ -0,0 +1,9 @@
+let cor = "azul";
+
+if (cor === "verde") {
+    console.log("siga");
+} else if (cor === "amarelo") {
+    console.log("atenção");
+} else if (cor === "verde") {
+    console.log("pare")
+}
